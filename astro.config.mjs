469da8c53cb84a119acce59131f1dcf09e177f54
Aryan <aryanmpandey@gmail.com>
2026-08-19 13:11:51 +0530

    hotfix: git username

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://shubhz-works.github.io',
+	site: 'https://shubz-works.github.io',
 });
